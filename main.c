35c8fdcf85f6e26b2616db52c7d8d70667a6d98e
Kachapin <kevinkachapin@gmail.com>
2022-12-16 03:16:47 +0300

    Update on main

--- main.c
+++ main.c
@@ -1,45 +1,90 @@
 #include "monty.h"
-bus_t bus = {NULL, NULL, NULL, 0};
+
+glob_t global;
+
 /**
-* main - monty code interpreter
-* @argc: number of arguments
-* @argv: monty file location
-* Return: 0 on success
+*main - entry of the program
+*
+*@argc: number of arguments
+*@argv: argument vectors
+*
+*Return: 0
 */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	char *content;
-	FILE *file;
-	size_t size = 0;
-	ssize_t read_line = 1;
-	stack_t *stack = NULL;
-	unsigned int counter = 0;
+	FILE *f;
+	char *line, **command;
+	size_t buffersize = 0;
+	unsigned int line_number = 1;
+	stack_t *head = NULL;
 
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	file = fopen(argv[1], "r");
-	bus.file = file;
-	if (!file)
+	f = fopen(argv[1], "r");
+	global.fd = f;
+	if (f == NULL)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	while (read_line > 0)
+	while (getline(&line, &buffersize, f) != -1)
 	{
-		content = NULL;
-		read_line = getline(&content, &size, file);
-		bus.content = content;
-		counter++;
-		if (read_line > 0)
+		global.line = line;
+		command = parser(line);
+		if (command == NULL)
+			continue;
+		if (command[0][0] == '#')
+			continue;
+		if (check_line(command) == -1)
 		{
-			execute(content, &stack, counter, file);
+			fprintf(stderr, "L%u: unknown instruction %s\n", line_number, command[0]);
+			fclose(f);
+			exit(EXIT_FAILURE);
 		}
-		free(content);
+		exec_cmd(command, line_number, &head);
+		line_number++;
 	}
-	free_stack(stack);
-	fclose(file);
-return (0);
+	free(command);
+	_free(&head);
+	fclose(f);
+	return (0);
+}
+/**
+*parser- parses the commmand input
+*
+*@buffer: string containing command
+*
+*Return: Parsed command
+*/
+char **parser(char *buffer)
+{
+	char **cmd, *token;
+	int i;
+
+	if (buffer == NULL)
+		return (NULL);
+
+	token = strtok(buffer, " \t\n\r");
+	if (token == NULL)
+		return (NULL);
+
+	cmd = malloc(sizeof(char *) * 1024);
+
+	if (cmd == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed");
+		fclose(global.fd);
+		exit(EXIT_FAILURE);
+	}
+	i = 0;
+	while (token)
+	{
+		cmd[i++] = token;
+		token = strtok(NULL, " \t\r\n");
+	}
+	cmd[i] = NULL;
+	return (cmd);
 }
